7ad65d33661cf7b13df6f1ef8335a956168c4cae
Nikhil Verma <nv5475609@gmail.com>
2023-07-26 01:23:31 +0530

    added global font to dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
+import { Press_Start_2P } from 'next/font/google'
+import { cn } from "@/lib/utils";
+
+const font = Press_Start_2P ({ weight: '400', subsets: ['latin'] });
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full relative text-white bg-black">
+    <div className={cn("h-full relative text-white bg-black", font.className)}>
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 ">
         <Sidebar />
         </div>
