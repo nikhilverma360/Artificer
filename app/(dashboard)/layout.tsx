befa44e1d0014e180c2696f6f179e3c7bd4ae05e
Nikhil Verma <nv5475609@gmail.com>
2023-07-26 01:04:16 +0530

    sidebar fix 🛠️

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,10 +3,10 @@ import { Sidebar } from "@/components/sidebar";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-black">
+    <div className="h-full relative text-white bg-black">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 ">
         <Sidebar />
-      </div>
+        </div>
       <main className="md:pl-72 pb-10">
         <Navbar />
         {children}
