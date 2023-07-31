8fd19da52aa0ec0df1a0f747370afc36e080f531
Nikhil Verma <nv5475609@gmail.com>
2023-07-31 17:11:05 +0530

    backgoround gradient

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,7 +4,7 @@ const LandingLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <main className="h-full bg-[#000000] overflow-auto">
+    <main className="h-full bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-black via-gray-800 to-black animate-text overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full w-full">
         {children}
       </div>
