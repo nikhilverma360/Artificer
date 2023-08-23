bb6131241ce9f4b7199ff9138f802bef834f69d0
Nikhil Verma <nv5475609@gmail.com>
2023-08-23 18:03:57 +0530

    Clerk Signup

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,13 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
-
+import { UserButton } from "@clerk/nextjs";
 const LandingPage = () => {
   return ( 
     <div className="h-full ">
       <LandingNavbar />
+      <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
       <LandingHero />
     </div>
    );
